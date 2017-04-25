d223127436b7cdeba9002d7cccfabb6b750beb8c
Nick Rebollo <nrebollo@cord.edu>
2017-04-24 21:35:07 -0500

    Implemented abstract methods from IRoom

--- AdventureGame/src/adventuregame/Pharmacy.java
+++ AdventureGame/src/adventuregame/Pharmacy.java
@@ -4,6 +4,26 @@ package adventuregame;
  *
  * @author Nick
  */
-public class Pharmacy {
+public class Pharmacy implements IRoom {
+
+    @Override
+    public String getRoomDescription() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setRoomDescription(String roomDescription) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getRoomTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setRoomTitle(String roomTitle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
